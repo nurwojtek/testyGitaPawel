b7ccb4a0cc3213bc07f673aac1dbfe73cd52bcc0
wojtek <galachwojciech@op.pl>
2023-04-05 19:20:29 +0200

    dodajmy for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int ileRazy = 4;
+        for (int i = 0; i < ileRazy; i++) {
+            System.out.println("*");
+        }
     }
 }
